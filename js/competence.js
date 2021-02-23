e9d86e11f20ecc6449fd2d9ca929ca667341634e
Llaplace <lisa.laplace12@gmail.com>
2021-02-23 19:53:57 +0100

    Ajout formations.html + beautify all files

--- js/competence.js
+++ js/competence.js
@@ -1,60 +1,59 @@
 $(document).ready(function () {
-//    console.log("ready !"); 
+  //    console.log("ready !"); 
 
-var competence = [
-    ["informatique","SQL","2 ans"],
-    ["informatique","C","2 ans"],
-    ["informatique","Perl","2 ans"],
-    ["langue","anglais","2 ans"],
-    ["langue","français","2 ans"],
-    ["biologie","NCBI","2 ans"],
-    ["biologie","génomique","2 ans"],
-]
+  var competence = [
+    ["informatique", "SQL", "2 ans"],
+    ["informatique", "C", "2 ans"],
+    ["informatique", "Perl", "2 ans"],
+    ["langue", "anglais", "2 ans"],
+    ["langue", "français", "2 ans"],
+    ["biologie", "NCBI", "2 ans"],
+    ["biologie", "génomique", "2 ans"],
+  ]
 
-// remplissage tableau
-$.each(competence, function(index,elt){
+  // remplissage tableau
+  $.each(competence, function (index, elt) {
     // console.log(elt);
-    var ligne = "<tr><td>"+elt[1]+"</td><td>"+elt[0]+"</td><td>"+elt[2]+"</td></tr>"
+    var ligne = "<tr><td>" + elt[1] + "</td><td>" + elt[0] + "</td><td>" + elt[2] + "</td></tr>"
     $("tbody").append(ligne);
-})
+  })
 
-//barre de recherche sur la première colonne
-$("#myInput").on("keyup", function() {
+  //barre de recherche sur la première colonne
+  $("#myInput").on("keyup", function () {
     var value = $(this).val().toLowerCase();
     var nbValue = 0;
-    $("#myTable tr").filter(function() {
+    $("#myTable tr").filter(function () {
       var isMatch = $(this).children(':eq(0)').text().toLowerCase().indexOf(value) > -1;
       $(this).toggle(isMatch)
-      
+
       if (isMatch) {
-        nbValue ++
+        nbValue++
       }
     });
-      //Si pas de résultat on affiche le texte
-      if(nbValue == 0){
-        $("#zeroCompetence").show();
-      }else{
-        console.log("ok");
-        $("#zeroCompetence").hide();
-      }
+    //Si pas de résultat on affiche le texte
+    if (nbValue == 0) {
+      $("#zeroCompetence").show();
+    } else {
+      console.log("ok");
+      $("#zeroCompetence").hide();
+    }
   });
 
 });
 
 //Filtre sur le type avec radio button
-$(":radio").change(function() {
-  if(this.checked) {
-      //console.log(this.value.toLowerCase())
-      var value = this.value.toLowerCase();
-      if(value == "tout"){
-        $("#myTable tr").show()
-      }
-      else{
-        //filtre sur la 2eme colonne
-        $("#myTable tr").filter(function() {
-          $(this).toggle($(this).children(':eq(1)').text().toLowerCase().indexOf(value) > -1)
-        });
-      }
+$(":radio").change(function () {
+  if (this.checked) {
+    //console.log(this.value.toLowerCase())
+    var value = this.value.toLowerCase();
+    if (value == "tout") {
+      $("#myTable tr").show()
+    } else {
+      //filtre sur la 2eme colonne
+      $("#myTable tr").filter(function () {
+        $(this).toggle($(this).children(':eq(1)').text().toLowerCase().indexOf(value) > -1)
+      });
+    }
 
   }
 });
