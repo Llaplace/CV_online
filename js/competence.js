cf69f1c5d5620575a3d22d455638f2824643654b
Llaplace <lisa.laplace12@gmail.com>
2021-02-23 14:08:20 +0100

    Barre de recherche ajoutée (js+html)

--- js/competence.js
+++ js/competence.js
@@ -11,11 +11,19 @@ var competence = [
     ["biologie","génomique","2 ans"],
 ]
 
+// remplissage tableau
 $.each(competence, function(index,elt){
-    console.log(elt);
+    // console.log(elt);
     var ligne = "<tr><td>"+elt[1]+"</td><td>"+elt[0]+"</td><td>"+elt[2]+"</td></tr>"
     $("tbody").append(ligne);
 })
 
+//barre de recherche sur la première colonne
+$("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).children(':eq(0)').text().toLowerCase().indexOf(value) > -1)
+    });
+  });
 
 });
